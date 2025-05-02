dd9f2849c2bdb76f5829af188c02349f69123a46
Krishna Bhatt <bhatt413@umn.edu>
2025-05-02 18:00:50 -0500

    Add SQL scripts for Customers, Employees, Inventory, Orders, Payments, and Publishers tables

--- DATABASE_SCRIPTS/Inventory.sql
+++ DATABASE_SCRIPTS/Inventory.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS Inventory (
     InventoryID INT  PRIMARY KEY,
-    BookID      INT NOT NULL,
     Quantity    INT NOT NULL,
     RestockThreshold INT NOT NULL,
-    Location    VARCHAR(100) NOT NULL,
-    FOREIGN KEY (BookID) REFERENCES Books(BookID)
+    Location    VARCHAR(100) NOT NULL
 ); 
