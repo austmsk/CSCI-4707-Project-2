cb22295a2603fde64a1fa7a323021be9061c383b
Krishna Bhatt <bhatt413@umn.edu>
2025-05-02 17:05:41 -0500

    books

---
+++ DATABASE_SCRIPTS/Book.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Book (
+    BookID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(200) NOT NULL,
+    Genre VARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    PublicationYear YEAR,
+    StockQuantity INT,
+    PublisherID INT,
+    FOREIGN KEY (PublisherID) REFERENCES Publishers(PublisherID)
+);
